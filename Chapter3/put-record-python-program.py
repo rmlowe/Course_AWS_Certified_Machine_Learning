f98f74a7a81311f94087790af7b9c3d1fed48262
Robert Lowe <robertmlowe@rmlowe.com>
2019-05-24 13:09:29 +0800

    Streaming Data Collection Lab

--- Chapter3/put-record-python-program.py
+++ Chapter3/put-record-python-program.py
@@ -5,14 +5,14 @@ import time
 import random
 import json
 
-client = boto3.client('kinesis', region_name='<INSERT_YOUR_REGION>')
+client = boto3.client('kinesis', region_name='us-east-1')
 partition_key = str(uuid.uuid4())
 
 while True:
         r = requests.get('https://randomuser.me/api/?exc=login')
         data = json.dumps(r.json())
         client.put_record(
-                StreamName='<INSERT_YOUR_STREAM_NAME>',
+                StreamName='streaming-data-collection-lab',
                 Data=data,
                 PartitionKey=partition_key)
         time.sleep(random.uniform(0, 1))
